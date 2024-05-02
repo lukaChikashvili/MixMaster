d8a22a9b007d2e0c880235bd0c26abe990a44160
luka chikashvili <lukachikashvili2020@gmail.com>
2024-05-02 19:41:40 +0400

    add canvas

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+      <mesh>
+        <boxGeometry />
+        <meshNormalMaterial />
+      </mesh>
+    </>
+  )
+}
+
+export default Main
