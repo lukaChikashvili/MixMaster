202eb96680bf8b956f75fbdc4c2843c943acd2c9
luka chikashvili <lukachikashvili2020@gmail.com>
2024-05-12 19:11:27 +0400

    add base color change with color picker

--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { MeshContext } from '../context/meshContext'
+import { HexColorPicker } from 'react-colorful';
 
 const Right = () => {
  
-  const { updateMeshColor } = useContext(MeshContext);
+  const { updateMeshColor, meshColor } = useContext(MeshContext);
+
+  const [showRGB, setShowRGB] = useState(false);
 
 
   const handleColorChange = (color) => {
@@ -12,17 +15,25 @@ const Right = () => {
   return (
     <div className='absolute right-12'>
        <h1 className='text-white p-6 cursor-pointer text-xl'>Choose mesh material</h1>
-       <div className='flex gap-4'>
-          <div className='w-6 h-6 rounded-full bg-red-500 border-2 duration-500 ease-in hover:border-white' onClick={() => handleColorChange('red')} ></div>
-          <div className='w-6 h-6 rounded-full bg-blue-500 border-2 duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('blue')}></div>
-          <div className='w-6 h-6 rounded-full bg-green-500 border-2 duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('green')}></div>
-          <div className='w-6 h-6 rounded-full bg-purple-500 border-2 duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('purple')}></div>
-          <div className='w-6 h-6 rounded-full bg-orange-500 border-2 duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('orange')}></div>
-          <div className='w-6 h-6 rounded-full bg-pink-500 border-2 duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('pink')}></div>
+       <div className='flex gap-4 pb-6'>
+          <div className='w-6 h-6 rounded-full bg-red-500 border-2 cursor-pointer duration-500 ease-in hover:border-white' onClick={() => handleColorChange('red')} ></div>
+          <div className='w-6 h-6 rounded-full bg-blue-500 border-2 cursor-pointer duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('blue')}></div>
+          <div className='w-6 h-6 rounded-full bg-green-500 border-2 cursor-pointer duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('green')}></div>
+          <div className='w-6 h-6 rounded-full bg-purple-500 border-2 cursor-pointer duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('purple')}></div>
+          <div className='w-6 h-6 rounded-full bg-orange-500 border-2 cursor-pointer duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('orange')}></div>
+          <div className='w-6 h-6 rounded-full bg-pink-500 border-2 cursor-pointer duration-500 ease-in hover:border-white'  onClick={() => handleColorChange('pink')}></div>
         </div>
 
-       <span className='text-white underline underline-offset-4'>Base color: </span>
+     <div className='flex gap-4 items-center '>
+       <span className='text-white underline underline-offset-4 '>Base color: </span> 
+       <div className={`w-24 h-6  rounded-md cursor-pointer`} style={{backgroundColor: !meshColor ? 'white' : meshColor}} onClick={() => setShowRGB(!showRGB)}></div>
        
+       </div>
+       {showRGB && (
+          <div className='mt-6'>
+            <HexColorPicker color = {meshColor} onChange={updateMeshColor} />
+          </div>
+       )}
     </div>
   )
 }
